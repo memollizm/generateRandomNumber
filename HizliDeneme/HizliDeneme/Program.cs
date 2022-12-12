76636aa8ea8da5f50cfe015e7043d5215c87250a
Mehmet Başgöze <92752077+memollizm@users.noreply.github.com>
2022-12-12 19:19:32 +0300

    Add files via upload

---
+++ HizliDeneme/HizliDeneme/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            double num = random.NextDouble();
+            Console.WriteLine(num);
+            Console.ReadKey();
+        }
+    }
+}
